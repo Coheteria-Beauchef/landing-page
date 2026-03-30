823c093eb0449e3bfe4fa9106a56f9af03501e50
Sergio Romero Véliz <sergioromeroiv@gmail.com>
2026-03-30 13:37:18 -0300

    fix: assets url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://coheteria-beauchef.github.io",
+  base: "/landing-page",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
