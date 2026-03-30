031310ee8e279f8f49575dbd128e91032d2dc4c8
Sergio Romero Véliz <sergioromeroiv@gmail.com>
2026-03-30 13:22:55 -0300

    add: pipeline gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://coheteria-beauchef.github.io",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
