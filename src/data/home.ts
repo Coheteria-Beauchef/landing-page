d14e28125b003caedd7f45f5c4e906ecb503175e
Matías Videla Meisenbichler <128327527+videlameisenb@users.noreply.github.com>
2026-05-07 17:43:12 -0400

    Update home.ts

--- src/data/home.ts
+++ src/data/home.ts
@@ -256,8 +256,8 @@ export const sponsorLogos: SponsorLogo[] = [
   { src: `${baseUrl}auspiciadores/maquintel_logo.webp`, alt: "Maquintel", heightClass: "h-[100px]" },
   { src: `${baseUrl}auspiciadores/alutipo_logo.webp`, alt: "Alutipo" },
   { src: `${baseUrl}auspiciadores/micro.webp`, alt: "Micro Automación" },
-  { src: `${baseUrl}auspiciadores/logo_ansys.webp`, alt: "Ansys", heightClass: "h-[70px]" },
-  { src: `${baseUrl}auspiciadores/logo_esss.webp`, alt: "ESSS", heightClass: "h-[70px]" },
+  { src: `${baseUrl}auspiciadores/logo_ansys.webp`, alt: "Ansys", heightClass: "h-[50px]" },
+  { src: `${baseUrl}auspiciadores/logo_esss.webp`, alt: "ESSS", heightClass: "h-[50px]" },
 ];
 
 export const socialLinks: SocialLink[] = [
